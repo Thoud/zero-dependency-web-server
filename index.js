54e9946686f91e6d19f263204cbbcd82231716e6
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-01-22 15:12:35 +0100

    Add routing for index.html and index.css

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+// Dependencies
 const http = require('http');
+const fs = require('fs');
 
+// Server with all routings
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('Hello World!');
-  res.end();
+  // Basic route for serving index.html
+  if (req.url === '/' || req.url === '/index.html') {
+    fs.readFile('./public/index.html', (error, data) => {
+      // Throwing an error if one comes up
+      if (error) throw error;
+      // Serving the data to the client
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    });
+  }
+
+  // Route for serving index.css
+  if (req.url === '/index.css') {
+    fs.readFile('./public/index.css', (error, data) => {
+      if (error) throw error;
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.write(data);
+      res.end();
+    });
+  }
 });
 
+// Initiate the server on Port 3000 and returning a message
 server.listen(3000);
-
 console.log('Web Server listening on port 3000...');
