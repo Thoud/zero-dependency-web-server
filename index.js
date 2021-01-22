4d92f12c7c021ebcb832ed46960f642770bf1712
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-01-22 15:27:48 +0100

    Add routing for memes folder and unknown path

--- index.js
+++ index.js
@@ -17,13 +17,43 @@ const server = http.createServer((req, res) => {
   }
 
   // Route for serving index.css
-  if (req.url === '/index.css') {
+  else if (req.url === '/index.css') {
     fs.readFile('./public/index.css', (error, data) => {
       if (error) throw error;
       res.writeHead(200, { 'Content-Type': 'text/css' });
       res.write(data);
       res.end();
     });
+  }
+
+  // Route for serving the index.html inside of the memes folder
+  else if (
+    req.url === '/memes' ||
+    req.url === '/memes/' ||
+    req.url === '/memes/index.html'
+  ) {
+    fs.readFile('./public/memes/index.html', (error, data) => {
+      if (error) throw error;
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    });
+  }
+
+  // Route for serving the image inside of the memes folder
+  else if (req.url === '/memes/1.jpg') {
+    fs.readFile('./public/memes/1.jpg', (error, data) => {
+      if (error) throw error;
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.write(data);
+      res.end();
+    });
+  }
+
+  // Route for an invalid request
+  else {
+    res.writeHead(404);
+    res.end();
   }
 });
 
